34fb153d0dfad31b5f00936ac439413b226907b7
Jason Liu <jliu@lcs.on.ca>
2019-04-13 10:11:11 -0400

    Got the key to look like it's being pressed

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -42,17 +42,17 @@ public class Piano extends World
         if ( (frames % 60 == 0) && (frames / 60 < 16) )
         {
             //Only show a message when we are in the bounds of the array
-            showText("Array Index is:" + frames / 60, 400, 250);
+            //showText("Array Index is:" + frames / 60, 400, 250);
 
             //Only say hello when we are in the bounds of the array
             // Say hello to everyone in the class!
-            showText("Hello " + peopleInClass[frames/60], 400, 170);
+            //showText("Hello " + peopleInClass[frames/60], 400, 170);
         }
 
         //Track frames
         frames += 1;
 
         //Show the frames
-        showText("" + frames, 100, 100);
+        //showText("" + frames, 100, 100);
     }
 }
