0fdf0d29933dadb41d31df0fc2bf82d54aa865d3
Jason Liu <jliu@lcs.on.ca>
2019-04-16 13:18:15 -0400

    finishing the arrays of sound and key

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -6,14 +6,24 @@ public class Key extends Actor
      * Instance variables
      * These can be used anywhere in the class below.
      */
-    //Will keep track of whether a key was just pressed
+    // Will keep track of whether a key was just pressed
     private boolean keyAlreadyDown;
-
-    public Key()
+    // Will contain the key and sound file that this instance of the class will play
+    private String key;
+    private String sound;
+    
+    /**
+     * Create a new key.
+     * Constuctor – it runs once when a Key object is created.
+     */
+    public Key(String keyName, String soundFile)
     {
-        //No key has been pressed
+        // No key has been pressed
         keyAlreadyDown = false;
-
+        
+        // Set the instance variables using the parameters
+        key = keyName;
+        sound = soundFile;
     }
 
     /**
@@ -22,35 +32,50 @@ public class Key extends Actor
     public void act()
     {
         // Animate the piano key being pressed
-        // Condition 1 - is the "g" key being pressed
-        // Condition 2 - was the "g" key already down
-        if ( Greenfoot.isKeyDown("g") && !keyAlreadyDown )
+        // Condition 1 – is the "g" key being pressed
+        // Condition 2 - the key was NOT already down
+        if ( Greenfoot.isKeyDown(key) && !keyAlreadyDown)
         {
-            //Key is down
+            // Key is down
             setImage("white-key-down.png");
             play();
             keyAlreadyDown = true;
         }
-        
+
         // Stop showing the key being pressed
         // Condition 1 - The key was down the last time act() fired
         // Condition 2 - The "g" has been released (no longer being pressed)
-        if ( keyAlreadyDown == true && !Greenfoot.isKeyDown("g") )
+        if ( keyAlreadyDown == true && !Greenfoot.isKeyDown(key))
         {
-            //Key is up
-            setImage("white-key.png"); 
+            // Key is up
+            setImage("white-key.png");
             keyAlreadyDown = false;
         }
-
+        
     }
 
     /**
-     * Play the note of this key
+     * Play the note of this key.
      */
     public void play()
     {
-        Greenfoot.playSound("3a.wav");
+        Greenfoot.playSound(sound + ".wav");
     }
-
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -8,18 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
  */
 public class Piano extends World
 {
-    /*
-     * Instance varaiables
+    /**
+     * Instance variables
      * (we can use these variables anywhere in the class)
      */
     int frames;
 
-    //Create the array (list of names)
-    String[] peopleInClass = {"Julio", "Liam", "Osman", "Simon", "Eric",
-            "Phillip", "Joven", "Joe", "Gordon", "Sam", "Jeff", "Jason", "Selena",
-            "Selena", "Jieun", "Nick"
+    // Create the array (list of names)
+    String[] peopleInClass = {"Julio", "Liam", "Dima", "Osman", "Nick", "Simon", "Eric", "Phillip", 
+            "Joven", "Joe", "Gordon", "Jieun", "Selena", "Jason", "Sam", "Jeff"
         };
 
+    // Create two arrays
+    // First array tracks the keyboard keys that we will use for white piano keys
+    String[] whiteKeys = {"a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "//"};
+    // Second array tracks the sounds files for each note
+    String[] whiteNotes = {"3c","3d","3e","3f","3g","3a","3b","4c","4d","4e","4f","4g"};
     /**
      * Make the piano.
      */
@@ -27,32 +31,36 @@ public class Piano extends World
     {
         super(800, 340, 1);
 
-        //Initialize instance variables
+        // Initialize instance variables
         frames = 0;
     }
 
-    /*
+    /**
      * act
-     * (gets called repeatedly to create animation
+     * (gets called repeatedly to create animation)
      */
-
     public void act()
     {
-        //Say "hello" to a different person each second
-        if ( (frames % 60 == 0) && (frames / 60 < 16) )
+        // This block of code runs once per second until the end of the
+        // array
+        if ( (frames % 60 == 0) && (frames / 60 < 15) )
         {
-            //Only show a message when we are in the bounds of the array
-            //showText("Array Index is:" + frames / 60, 400, 250);
+            // Only show a message when we are in the bounds of the array
+            // showText("Array index is: " + frames / 60, 400, 250);
 
-            //Only say hello when we are in the bounds of the array
-            // Say hello to everyone in the class!
-            //showText("Hello " + peopleInClass[frames/60], 400, 170);
+            // Only say hello when we are in the bounds of the array
+            // Say hello to everyone in the class! 
+            // showText("Hello " + peopleInClass[frames / 60], 400, 170);
+
         }
 
-        //Track frames
+        // Track frames
         frames += 1;
 
-        //Show the frames
-        //showText("" + frames, 100, 100);
+        // Show the frames
+        // showText("" + frames, 100, 100);
     }
+
 }
+
+
